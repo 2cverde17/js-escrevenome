1a5a1834b30807503ee3842f738495084b184e8b
2cverde17 <168051924+2cverde17@users.noreply.github.com>
2024-04-25 00:56:12 -0300

    added js and css files

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//variables
+let BallX = 300 
+let BallY = 200
+let Diameter = 13
+let Radius = Diameter/2
+let collided = false
+let multiplayer = 2
+let ErrorChance = 0
+
+//sounds
+let raquetada
+let ponto
+let trilha
+
+//score
+let PlayerPoints = 21
+let EnemyPoints = 0
+
+//enemy variables
+let EnemyRacketX = 585
+let EnemyRacketY = 150
+let EnemyYVelocity
+
+//Speed
+let VelocityBallX = 6
+let VelocityBallY = 6
+
+//racket variables
+RacketX = 5
